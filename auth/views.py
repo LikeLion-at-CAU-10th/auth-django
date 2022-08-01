ab9258180dcdd35b489fd455eed663c154e120a6
guri <safg0421@splab.dev>
2022-08-02 02:19:40 +0900

    로그인 성공 시 유저정보 응답

--- auth/views.py
+++ auth/views.py
@@ -11,9 +11,19 @@ def login_view(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return JsonResponse({"success": True, "message": "로그인 성공"}, status=200)
+        user_data = {
+            "id": user.pk,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_active": user.is_active,
+            "date_joined": user.date_joined,
+            "last_login": user.last_login
+        }
+        return JsonResponse({"success": True, "message": "로그인 성공", "data": user_data}, status=200)
     else:
         return JsonResponse({"success": False, "message": "로그인 실패"}, status=403)
+
 
 def logout_view(request):
     logout(request)
